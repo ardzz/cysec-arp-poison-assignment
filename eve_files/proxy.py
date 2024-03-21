43dca945cffbb3d16d47733e1b9fe83806aa052f
Naufal Reky Ardhana <rekyardhana029@gmail.com>
2024-03-21 14:22:24 +0700

    rename from eve to hecker

--- eve_files/proxy.py
+++
@@ -1,4 +0,0 @@
-from mitmproxy import http
-
-def response(flow: http.HTTPFlow) -> None:
-    flow.response.content = flow.response.content.decode().replace("Helloooo! I'm an honest website!", "You've been PWN'D! I'm an evil website").encode()
